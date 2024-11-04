dd431a702752f7c0c1de325cf12553899743a8cc
Patrick Busenius <patrick.busenius@gmai.com>
2024-11-04 11:06:32 +0100

    added env loading

--- main.py
+++ main.py
@@ -1,6 +1,34 @@
 
+import os
+
+from tqdm import tqdm
+from dotenv import load_dotenv
+
+from src.errors.env import MissingEnvironmentVariable
+
+
+load_dotenv()
+
+
+def load_env(name: str) -> str:
+    value = os.getenv(name)
+
+    print(value)
+
+    if value != "":
+        return value
+
+    raise MissingEnvironmentVariable(f"Enviroment variable'{name}' does not exist")
+
+
+
 def main():
-    pass
+    # ais simplification
+
+    ais_input_directory = load_env("AIS_INPUT_DIRECTORY")
+
+    for i in tqdm(os.listdir(ais_input_directory)):
+        print(i)
 
 if __name__ == "__main__":
     main()
