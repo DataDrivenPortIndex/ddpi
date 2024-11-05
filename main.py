9e540aa305091ab78b126315b836bc2287f87872
pbusenius <patrick.busenius@gmail.com>
2024-11-05 16:11:06 +0100

    added debug information and error handling

--- main.py
+++ main.py
@@ -16,8 +16,8 @@ load_dotenv(override=True)
 def get_enviroment_variable(name: str) -> str:
     value = os.getenv(name)
 
-    if value != "" and "XXXXX" not in value:
-        print(f"\tEnviroment variable '{name}' set to:\t {value}")
+    if value is not None and value != "" and "XXXXX" not in value :
+        print(f"\tEnviroment variable '{name}' set to:\t\t {value}")
         return value
 
     raise MissingEnvironmentVariable(f"Enviroment variable'{name}' does not exist")
@@ -38,21 +38,25 @@ def extraction(input_directory: str, output_directory: str, years: str):
             if year in day_file:
                 if os.path.isfile(output_file):
                     continue
+                try:
 
-                day_file_path = os.path.join(input_directory, day_file)
+                    day_file_path = os.path.join(input_directory, day_file)
 
-                df_lazy = event_extraction.process_day(day_file_path)
+                    df_lazy = event_extraction.process_day(day_file_path)
 
-                dfs.append(df_lazy)
+                    dfs.append(df_lazy)
+                except pl.exceptions.ComputeError:
+                    print(day_file)
+                    pass
 
-            if not os.path.isfile(output_file):
-                pl.concat(dfs).write_parquet(output_file)
+        if not os.path.isfile(output_file):
+            pl.concat(dfs).write_parquet(output_file)
 
 
 def validation(input_directory: str, output_directory: str):
     for port_event_file in tqdm(os.listdir(input_directory)):
         event_file_path = os.path.join(input_directory, port_event_file)
-        validated_event_file_path = os.path.join(output_directory, f"validated_{port_event_file}")
+        validated_event_file_path = os.path.join(output_directory, f"validated_{port_event_file.split('.')[0]}.csv")
         
         event_validation.validate(event_file_path, validated_event_file_path)
 
@@ -62,23 +66,24 @@ def main():
     ais_input_directory = get_enviroment_variable("AIS_INPUT_DIRECTORY")
     ais_simplified_directory = get_enviroment_variable("AIS_SIMPLIFIED_DIRECTORY")
     ais_port_events_directory = get_enviroment_variable("AIS_PORT_EVENTS_DIRECTORY")
+    ais_validated_port_events_directory = get_enviroment_variable("AIS_VALIDATED_PORT_EVENTS_DIRECTORY")
 
     print("Processing\n")
 
-    print("Pre-Processing\n")
+    print("Data Preparation")
 
-    print("Perform AIS-Simplification:")
+    print("\tPerform AIS-Simplification:")
     simplification(ais_input_directory, ais_simplified_directory)
 
-    print("Port-Event-processing\n")
+    print("Port-Event-Processing")
 
-    print("Perform Port-Event-Extraction:")
+    print("\tPerform Port-Event-Extraction:")
     extraction(ais_simplified_directory, ais_port_events_directory, ["2020"])
 
-    print("Perform Port-Event-Validation:")
-    validation(ais_port_events_directory, ".")
+    print("\tPerform Port-Event-Validation:")
+    validation(ais_port_events_directory, ais_validated_port_events_directory)
 
-    print("Perform Port-Clustering:")
+    print("\tPerform Port-Clustering:")
 
 
 if __name__ == "__main__":
