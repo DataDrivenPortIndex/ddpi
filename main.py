5e5b7ad2978cafe6e202ca5b2c77c304d6b246cf
Patrick Busenius <patrick.busenius@gmai.com>
2024-11-05 10:57:56 +0100

    added port-event extraction

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 
 import os
+import polars as pl
 
 from tqdm import tqdm
 from dotenv import load_dotenv
 
+from src.port_detection import event_extraction
 from src.pre_processing import ais_simplification
 from src.errors.env import MissingEnvironmentVariable
 
@@ -23,9 +25,21 @@ def get_env(name: str) -> str:
 
 def simplification(input_directory: str, output_directory: str):
     for day_file in tqdm(os.listdir(input_directory)):
-        input_directory = os.path.join(input_directory, day_file)
-        ais_simplification.simplifiy(input_directory, output_directory)
+        day_file_path = os.path.join(input_directory, day_file)
+        ais_simplification.simplifiy(day_file_path, output_directory)
 
+def extraction(input_directory: str, output_directory: str, year: str):
+    dfs = []
+    for day_file in tqdm(os.listdir(input_directory)):
+        day_file_path = os.path.join(input_directory, day_file)
+
+        df = event_extraction.process_day(day_file_path)
+    
+        dfs.append(df.collect())
+
+    output_file = os.path.join(output_directory, f"port_events_{year}.parquet")
+
+    pl.concat(dfs).write_parquet(output_file)
 
 def main():
     # ais simplification
@@ -36,6 +50,8 @@ def main():
     simplification(ais_input_directory, ais_simplified_directory)
 
     print("Perform Port-Event-Extraction:")
+    extraction(ais_simplified_directory, )
+
 
     print("Perform Port-Event-Validation:")
 
