c69cb6d4aa787f1218eb5cb143812af05b70b40d
pbusenius <patrick.busenius@gmail.com>
2024-11-06 13:44:54 +0100

    added id to export

--- src/port_detection/export.py
+++ src/port_detection/export.py
@@ -1,4 +1,3 @@
-import pandas as pd
 import geopandas as gpd
 
 from h3ronpy.pandas.vector import geodataframe_to_cells
@@ -8,10 +7,14 @@ from h3ronpy import ContainmentMode
 H3_POLYFILL_MODE = ContainmentMode.Covers
 
 
-def export_as_h3(gdf: gpd.GeoDataFrame, resolution: int) -> pd.DataFrame:
+def as_h3_csv(gdf: gpd.GeoDataFrame, file_name: str,  resolution: int):
     df_ddpi = geodataframe_to_cells(gdf, resolution, containment_mode=H3_POLYFILL_MODE)
 
     df_ddpi["cell"] = df_ddpi["cell"].apply(hex)
     df_ddpi["cell"] = df_ddpi["cell"].str.replace("0x", "")
 
-    return df_ddpi
+    df_ddpi.to_csv(file_name, index=False)
+
+
+def as_csv(gdf: gpd.GeoDataFrame, file_name: str):
+    gdf.to_csv(file_name, index=False)
