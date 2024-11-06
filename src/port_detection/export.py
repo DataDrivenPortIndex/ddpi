908c7f7e71a453444d2de0ca23c534abf91f3376
pbusenius <patrick.busenius@gmail.com>
2024-11-06 13:15:01 +0100

    added const

--- src/port_detection/export.py
+++ src/port_detection/export.py
@@ -5,8 +5,11 @@ from h3ronpy.pandas.vector import geodataframe_to_cells
 from h3ronpy import ContainmentMode
 
 
+H3_POLYFILL_MODE = ContainmentMode.Covers
+
+
 def export_as_h3(gdf: gpd.GeoDataFrame, resolution: int) -> pd.DataFrame:
-    df_ddpi = geodataframe_to_cells(gdf, 10, containment_mode=ContainmentMode.Covers)
+    df_ddpi = geodataframe_to_cells(gdf, resolution, containment_mode=H3_POLYFILL_MODE)
 
     df_ddpi["cell"] = df_ddpi["cell"].apply(hex)
     df_ddpi["cell"] = df_ddpi["cell"].str.replace("0x", "")
